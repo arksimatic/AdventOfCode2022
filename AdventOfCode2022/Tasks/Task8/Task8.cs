37c7de396f60fe1f81238ccfe75283bf6141965e
astisk <pobierac@gmail.com>
2022-12-08 16:01:36 +0100

    Task8

---
+++ AdventOfCode2022/Tasks/Task8/Task8.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2022.Tasks.Task8
+{
+    class Task8 : TaskBase
+    {
+        public override int TaskNumber => 8;
+
+        public override void Solve()
+        {
+            // Get data
+            Tree[] trees = LoadData();
+
+            // First half
+            Answer1 = trees.Where(tree => tree.IsVisible).Count().ToString();
+
+            // Second half
+            Answer2 = trees.Select(tree => tree.MultiplicationOfVisibleTrees).OrderByDescending(treeValue => treeValue).First().ToString();
+
+            // Save data
+            SaveData();
+        }
+
+        public Tree[] LoadData()
+        {
+            String[] splittedData = Data.Split("\r\n");
+
+            Int32 rows = splittedData[0].Length;
+            Int32 columns = splittedData.Length;
+
+            List<Tree> trees = new List<Tree>();
+
+            for(int row = 0; row < rows; row++)
+            {
+                for(int col = 0; col < columns; col++)
+                {
+                    (Int32 row, Int32 col) position = (row, col);
+
+                    List<Int32> left = new List<Int32>();
+                    for (int l = position.col - 1; l >= 0; l--)
+                        left.Add(splittedData[position.row][l]);
+
+                    List<Int32> right = new List<Int32>();
+                    for (int r = position.col + 1; r < columns; r++)
+                        right.Add(splittedData[position.row][r]);
+
+                    List<Int32> up = new List<Int32>();
+                    for (int u = position.row - 1; u >= 0; u--)
+                        up.Add(splittedData[u][position.col]);
+
+                    List<Int32> down = new List<Int32>();
+                    for (int d = position.row + 1; d < rows; d++)
+                        down.Add(splittedData[d][position.col]);
+
+                    trees.Add(new Tree()
+                    {
+                        Height = Convert.ToInt32(splittedData[position.row][position.col]),
+                        TreesOnLeftHeights = left.ToArray(),
+                        TreesOnRightHeights = right.ToArray(),
+                        TreesOnUpHeights = up.ToArray(),
+                        TreesOnDownHeights = down.ToArray()
+                    });
+                }
+            }
+
+            return trees.ToArray();
+        }
+    }
+}
---
+++ AdventOfCode2022/Tasks/Task8/Tree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2022.Tasks.Task8
+{
+    class Tree
+    {
+        public Int32 Height { get; set; }
+        public Int32[] TreesOnLeftHeights { get; set; }
+        public Int32[] TreesOnRightHeights { get; set; }
+        public Int32[] TreesOnUpHeights { get; set; }
+        public Int32[] TreesOnDownHeights { get; set; }
+
+        public Boolean IsVisibleFromLeft => !TreesOnLeftHeights.Where(height => height >= Height).Any();
+        public Boolean IsVisibleFromRight => !TreesOnRightHeights.Where(height => height >= Height).Any();
+        public Boolean IsVisibleFromUp => !TreesOnUpHeights.Where(height => height >= Height).Any();
+        public Boolean IsVisibleFromDown => !TreesOnDownHeights.Where(height => height >= Height).Any();
+        public Boolean IsVisible => IsVisibleFromLeft || IsVisibleFromRight || IsVisibleFromUp || IsVisibleFromDown;
+
+        public Int32 VisibleTreesOnLeft => VisibleTreesInDirection(TreesOnLeftHeights);
+        public Int32 VisibleTreesOnRight => VisibleTreesInDirection(TreesOnRightHeights);
+        public Int32 VisibleTreesOnUp => VisibleTreesInDirection(TreesOnUpHeights);
+        public Int32 VisibleTreesOnDown => VisibleTreesInDirection(TreesOnDownHeights);
+        public Int32 MultiplicationOfVisibleTrees => VisibleTreesOnLeft * VisibleTreesOnRight * VisibleTreesOnUp * VisibleTreesOnDown;
+
+        public Int32 VisibleTreesInDirection(Int32[] trees)
+        {
+            Int32 visibleTrees = 0;
+            foreach (var tree in trees)
+            {
+                visibleTrees++;
+                if (tree >= Height)
+                    break;
+            }
+
+            return visibleTrees;
+        }   
+    }
+}
